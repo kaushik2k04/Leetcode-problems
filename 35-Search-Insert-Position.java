d67b563056eed5a06cf6ffbde26c7ef6dea92d0f
kaushik2k04 <kskaushik031@gmail.com>
2025-01-06 10:31:04 +0530

    [35] [Time Beats: 100.00%] [Memory Beats: 8.75%] - LeetPush

---
+++ 35-Search-Insert-Position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+        if(nums[i]==target){
+            count= i;   
+        }
+        else if(nums[i] < target)
+            {
+                count=i+1;
+            }
+        
+            
+        }
+        return count; 
+    }
+      
+    
+}
